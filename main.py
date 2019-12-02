0802b644dcc245d1d568733839fa08a762601ddb
ronaldrosejoseph <32349696+ronaldrosejoseph@users.noreply.github.com>
2019-12-01 22:00:54 -0500

    Added the dynamic class

--- main.py
+++ main.py
@@ -1,29 +1,33 @@
 class Product:
-    def __init__(self, sno, name, height, barcode):
+    def __init__(self, *, sno, name, height, barcode, inventory=None):
         self.name = name
         self.sno = sno
         self.height = height
         self.barcode = barcode
+        self.inventory = inventory
 
     def show(self):
-        print(self.name, self.sno, self.height, self.barcode)
+        print(self.name, self.sno, self.height, self.barcode, self.inventory)
 
-
-# one = Product('Ron', 26)
-# one.show()
 
 row_data = 'S.no	1	2	3	Name	Juice	Water	Cookies	Height	180	170	190	Barcode	9780201379624	9780201379012	9771234567003'
 headers = ['S.no', 'Name', 'Height', 'Barcode', 'Inventory']
 data = row_data.split()
-
-print(data)
 no_rows = 4
+product = {}
 
-i = 0
-while i < len(data):
-    data[i] = Product(data[i], data[i+1])
-    data[i].show()
-    if i <= len(data) - no_rows + 1:
-        i += no_rows - 1
+for header in headers:
+    if header in row_data:
+        pos = data.index(header)
+        product[header] = data[pos+1:pos+no_rows]
+
+row = 0
+while row < no_rows-1:
+    product['Name'][row] = Product(sno=product['S.no'][row], name=product['Name'][row], height=product['Height'][row], barcode=product['Barcode'][row])
+    row += 1
+
+product['Name'][0].show()
+product['Name'][1].show()
+product['Name'][2].show()
 
 
